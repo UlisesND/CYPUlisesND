848f5c9ea2fa06bbca66799679df17d78400e1a6
UlisesND <ulikronno@gmail.com>
2019-09-24 22:20:03 -0500

    ejemplo1_15.py

---
+++ libro/ejemplo1_15.py
@@ -0,0 +1,6 @@
+BASE = float(input("Ingresar base de rectangulo"))
+ALTU = float(input("Ingrese la altura del rectangulo"))
+SUP = BASE * ALTU
+PER = 2 *(BASE + ALTU)
+print(f"La superficie del triangulo es de {SUP}")
+print(f"El perimetro del triangulo es de {PER}")
