65b6c63e9fd1c6a87a8de6fa93217e00f90eb61f
UlisesND <ulikronno@gmail.com>
2019-09-24 22:30:23 -0500

    ejemplo1_5.py

---
+++ libro/problemas_resueltos/capitulo1/ejemplo1_5.py
@@ -0,0 +1,6 @@
+RADIO = float(input(“Determine el radio del cilindro”))
+ALTU = float(input(“Determine la altura del cilindro”))
+VOL = 3.141592 * (RADIO ** 2) * ALTU
+ARE = 2 * 3.141592 * RADIO * ALTU
+print(f”El volumen del cilindro = π * (radio)**2 * altura, es: {VOL}”)
+print(f”La superficie de el cilindro = 2 * π * radio * altura, es de: {ARE}
