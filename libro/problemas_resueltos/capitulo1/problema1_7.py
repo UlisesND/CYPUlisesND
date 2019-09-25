a34653f5ea8dbf491f04f40117485a2b4ae69923
UlisesND <ulikronno@gmail.com>
2019-09-24 22:41:12 -0500

    problema1_7.py

---
+++ libro/problemas_resueltos/capitulo1/problema1_7.py
@@ -0,0 +1,6 @@
+L1 = float(input(“Determine el primer lado del triangulo:”)
+L2 = float(input(“Determine el segundo lado del triangulo:”)
+L3 = float(input(“Determine el tercer lado del triangulo:”)
+S = (L1 + L2 + L3)/2
+AREA = (5* (5 – L1) * (5 – L2) * (5 – L3)) ** O.5
+print(f”El área del triangulo con lados {L1}, {L2}, {L3}, es de: {AREA}”)
