5f7efae43ac455befe8cab80cbeecd9f993d8fce
UlisesND <ulikronno@gmail.com>
2019-09-24 22:29:20 -0500

    ejemplo1_4.py

---
+++ libro/problemas_resueltos/capitulo1/ejemplo1_4.py
@@ -0,0 +1,4 @@
+GAL = float(input(“Cuantos galones comprara”))
+TOTAL= GAL * 3.785 * 8.20
+print(f”Usted pidió {GAL} gal, el precio por gal es de: $ 8.20, por lo tanto su total a pagar es de: {TOTAL}”)
+
