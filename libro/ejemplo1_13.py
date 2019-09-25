0e81f3e715f559b4bad7be4043f0ecfeb946f23f
UlisesND <ulikronno@gmail.com>
2019-09-24 22:17:35 -0500

    ejemplo1_13.py

---
+++ libro/ejemplo1_13.py
@@ -0,0 +1,10 @@
+MAT = str(input("Ingrese las materias"))
+CAL1 = float(input("Ingrese la primera calificación"))
+CAL2 = float(input("Ingrese la segunda calificación"))
+CAL3 = float(input("Ingrese la tercera calificación"))
+CAL4 = float(input("Ingrese la cuarta calificación"))
+CAL5 = float(input("Ingrese la quinta calificación"))
+PRO = (CAL1 + CAL2 + CAL3 + CAL4 + CAL5 )/5
+print(f"La materia a promediar es:  { MAT }. ")
+print(f"El promedio obtenido es:  { PRO }. ")
+
