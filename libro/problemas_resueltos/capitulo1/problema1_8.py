1bb769d3ce9411e519abaf882940d5bf297f930f
UlisesND <ulikronno@gmail.com>
2019-09-24 22:42:04 -0500

    problema1_8.py

---
+++ libro/problemas_resueltos/capitulo1/problema1_8.py
@@ -0,0 +1,7 @@
+X1 = float(input(“Determine X1”)
+Y1 = float(input(“Determine Y1”)
+X2 = float(input(“Determine X2”)
+Y2 = float(input(“Determine Y2”)
+DIS = ((X1 – X2) * 2 + (Y1 – Y2) * 2) ** 0.5
+print(f”La distancia entre los puntos ({X1}, {Y1}) y ({X2}, {Y2}) es de: {DIS})
+
